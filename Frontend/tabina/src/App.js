76fd4a4a9da20371e8368fe47df3807de03c617e
Huydoan2003 <95405618+Huydoan2003@users.noreply.github.com>
2023-04-11 21:15:41 +0700

    Navbar

--- Frontend/tabina/src/App.js
+++ Frontend/tabina/src/App.js
@@ -1,30 +1,14 @@
-import logo from "./logo.svg";
-import "./App.css";
-  
-function App() {
+import React from 'react';
+import Navbar from './Components/Navbar';
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" 
-             alt="logo" />
-          
-<p>A simple React app.....</p>
-  
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <form action="../../post" method="post" 
-              className="form">
-          <button type="submit">Connected?</button>
-        </form>
-      </header>
+    <div>
+      <Navbar />
     </div>
-  );
-}
   
+  );
+};
+
 export default App;
