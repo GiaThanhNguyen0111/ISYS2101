e08e0f35003a392b26704a390bbffd93711ca1c2
Huydoan2003 <95405618+Huydoan2003@users.noreply.github.com>
2023-04-13 10:41:52 +0700

    Navbar and Footer

--- Frontend/tabina/src/App.js
+++ Frontend/tabina/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Navbar from './Components/Navbar';
+import Footer from './Components/Footer'
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
 
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Footer/>
     </div>
   
   );
--- Frontend/tabina/src/Components/Footer.js
+++ Frontend/tabina/src/Components/Footer.js
@@ -1,51 +1,66 @@
-// import { FaFacebookSquare, FaInstagramSquare, FaTwitterSquare } from 'react-icons/fa';
-// import { Container, Row, Col } from 'react-bootstrap';
-// import { Link } from 'react-router-dom';
-// export const Footer = () => {
-//   return (
-//     <Container fluid className="footer text-white pt-3">
-//       <Container className="text-md-start mt-2">
-//         <Row className="mt-3">
-//           <Col sm={6} md={3} className="mx-auto mb-4">
-//             <h5 className="text-uppercase fw-bold mb-4">
-//               Mentscape
-//             </h5>
-//             <p>
-//               Mentscape is an application focusing solely on mental healthcare services. Menscape allows people facing mental issues to connect with a variety of professional therapists easily.
-//             </p>
-//           </Col>
-//           <Col sm={6} md={3} className="mx-auto mb-4">
-//             <h5 className="text-uppercase fw-bold mb-4">
-//               About Mentscape
-//             </h5>
-//             <p><Link to="/" className="text-reset">History</Link></p>
-//             <p><Link to="/" className="text-reset">Administrator Team</Link></p>
-//             <p><Link to="/" className="text-reset">Our Network</Link></p>
-//             <p><Link to="/" href="../../pages/Privacy/thirdparty.php" className="text-reset">Operational Regulations</Link></p>
-//           </Col>
-//           <Col sm={6} md={3} className="mx-auto mb-4">
-//             <h5 className="text-uppercase fw-bold mb-4">Our policy</h5>
-//             <p><Link to="/" className="text-reset">Terms Of Use</Link></p>
-//             <p><Link to="/" className="text-reset">Data Collection</Link></p>
-//             <p><Link to="/" className="text-reset">Privacy Policy</Link></p>
-//             <p><Link to="/" className="text-reset">Frequently asked questions</Link></p>
-//           </Col>
-//           <Col sm={6} md={3} className="mx-auto mb-4">
-//             <h5 className="text-uppercase fw-bold mb-4">Contact Us</h5>
-//             <p>Location: 521 Kim Ma, Ha Noi, Viet Nam</p>
-//             <p>Mail: mentscape@gmail.com.vn</p>
-//             <p>Phone Number: +84 97382555</p>
-//             <div className="d-sm-flex justify-content-center pe-5">
-//               <FaFacebookSquare size='25' className="mx-2" />
-//               <FaInstagramSquare size='25' className="mx-2" />
-//               <FaTwitterSquare size='25' className="mx-2" />
-//             </div>
-//           </Col>
-//         </Row>
-//       </Container>
-//       <div className="footer-copyright text-center p-2">
-//         <p> © 2022 Copyright: <span className="fw-bold">Group 80 - BITS Team</span></p>
-//       </div>
-//     </Container>
-//   )
-// }
+import React from "react";
+import '../Css/footer.css';
+import tabina from '../Image/tabina.png'
+
+function Footer() {
+  return (
+    
+    <footer>
+      <div className="footer-columns" >
+        <div className="footer-column">
+          <img src= {tabina} className="tabina" />
+        </div>
+        <div className="footer-column">
+          <h3>Home</h3>
+          <ul>
+            <li>Find your recipe</li>
+            <li>Delicious recipe</li>
+            <li>Popular dishes</li>
+            <li></li>
+          </ul>
+          
+        </div>
+        <div className="footer-column">
+          <h3>Recipes</h3>
+          <ul>
+            <li>Mexican</li>
+            <li>Italy</li>
+            <li>Asian</li>
+            <li>Euroupean</li>
+          </ul>
+        </div>
+        <div className="footer-column">
+          <h3>About</h3>
+          <ul>
+            <li>Blog</li>
+            <li>FAQs</li>
+            <li>Terms of Service</li>
+            <li>Privacy Policy</li>
+          </ul>
+        </div>
+        <div className="footer-column">
+          <h3>Follow Us</h3>
+          <ul className="social-media-icons">
+            <li>
+              
+                <i className="fab fa-facebook-f"></i>
+            
+            </li>
+            <li>
+             
+                <i className="fab fa-twitter"></i>
+        
+            </li>
+            <li>
+              <a href="#">
+                <i className="fab fa-instagram"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
