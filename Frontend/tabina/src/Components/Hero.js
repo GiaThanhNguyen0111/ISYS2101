5a14d989050f34383c52d65d5a7f30e2479d1fe3
khue <khue251103@gmail.com>
2023-04-21 01:52:34 +0700

    Finish Hero section

--- Frontend/tabina/src/Components/Hero.js
+++ Frontend/tabina/src/Components/Hero.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import '../Css/hero.css';
-import bookImg from '../Image/Hero book image.png'
+import bookImg from '../Image/img/recipe/Hero book image.png';
+import curveShape from '../Image/img/recipe/Curve Shape.png';
+// import bookImg from '../Image/img/recipe/Tabina (3).png'
 
 function Hero() {
   return (
@@ -8,7 +10,7 @@ function Hero() {
 
       <div className="hero-left">
         <h1 className="hero-title">The best food for everyone</h1>
-        <p className="hero-subtitle">The Mina cooking community is nourished with famous chefs and <br></br>validated experts, get recipes and bring home tasty meals.</p>
+        <p className="hero-subtitle">The Mina cooking community is nourished with famous chefs and validated experts, get recipes and bring home tasty meals.</p>
         <div className='hero-buttons'>
           <button className="hero-cta" id='get-recipe'>GET RECIPES</button>
           <button className="hero-cta" id='share-recipe'>SHARE RECIPES</button>
@@ -16,7 +18,8 @@ function Hero() {
       </div>
 
       <div className='hero-right'>
-       <img src={bookImg} className='hero-img' alt='img'/>
+        <img src={curveShape} className='hero-curve' alt='curve'/>
+        <img src={bookImg} className='hero-img' alt='img'/>
       </div>
       
 
