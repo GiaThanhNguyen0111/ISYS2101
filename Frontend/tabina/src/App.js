0c8e15ddeba48ee05de93fcf5fcf3f3f4a8d5dae
BaoHoangRmit <s3926050@rmit.edu.vn>
2023-04-22 17:32:22 +0700

    few css to recipeItem

--- Frontend/tabina/src/App.js
+++ Frontend/tabina/src/App.js
@@ -3,13 +3,13 @@ import Chefcard from './Components/Chefcard';
 import Loginform from './Components/Login/Loginform';
 import Registerform from './Components/Login/Registerform';
 import RecipeItem from './Components/RecipeItem';
-
+import './Css/app.css';
 
 const App = () => {
   return (
     <>
-    <Chefcard />
-    {/* <RecipeItem /> */}
+    {/* <Chefcard /> */}
+    <RecipeItem />
     </>
   );
 };
--- Frontend/tabina/src/Components/RecipeItem.js
+++ Frontend/tabina/src/Components/RecipeItem.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import '../Css/recipeItem.css'
+import food from '../Image/img/recipe/salmon.jpg'
 
 const RecipeItem = () => {
     return (
         <>
-        <p>this is recipe item</p>
-        <p>this is recipe item to test git</p>
+        <div className='recipe-item'>
+            <div className='recipe-item-img'>
+                <img src = {food} className='recipe-item-img-food' />
+            </div>
+            <div className='recipe-item-body'>
+                <div className='recipe-item-body-title'>
+
+                </div>
+                <div className='recipe-item-body-rating'>
+
+                </div>
+                <div className='recipe-item-body-desc'>
+                    <div className='recipe-item-body-desc-text'>
+
+                    </div>
+                    <div className='recipe-item-body-desc-value'>
+
+                    </div>
+                </div>
+            </div>
+        </div>
         </>
     )
 
