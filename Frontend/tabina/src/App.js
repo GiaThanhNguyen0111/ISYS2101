4009e1a46ded965ec60dc40e6afeb43157841a8c
Huydoan2003 <95405618+Huydoan2003@users.noreply.github.com>
2023-05-03 00:04:41 +0700

    Searchbar

--- Frontend/tabina/src/App.js
+++ Frontend/tabina/src/App.js
@@ -7,6 +7,7 @@ import {
 } from 'react-router-dom';
 import RecipeDetail from './Pages/RecipeDetail';
 import Home from './Pages/Home';
+import Searchbar from './Components/Searchbar';
 
 const App = () => {
   return (
@@ -19,6 +20,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/recipes/:recipeId" element={<RecipeDetail />} />
+        <Route path='/search' element={<Searchbar/>} />
       </Routes>
     </>
   );
---
+++ Frontend/tabina/src/Components/Searchbar.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import '../Css/searchbar.css';
+
+
+
+
+const Searchbar = () => {
+  const [relavance, setRelavance] = useState('');
+
+
+  const handleRelavance = (event) => {
+    setRelavance(event.target.value);
+  };
+
+
+  return (
+    <>
+      <div className="searchbar-container">
+     
+        <input type="text" placeholder="Add Ingredients" className='searchbar' />
+        <button className='searchbtn'>+</button>
+       
+        <select value={relavance} onChange={handleRelavance}  className='relchoice'>
+          <option value="">Relavance</option>
+          <option value="option1">Option 1</option>
+          <option value="option2">Option 2</option>
+          <option value="option3">Option 3</option>
+        </select>
+
+
+      </div>
+    </>
+  );
+};
+
+
+export default Searchbar;
